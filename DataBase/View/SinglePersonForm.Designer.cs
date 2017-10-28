d86d8ddf714f0352af69c88e87629066e84f9806
Надежда Бердник <nadya.berdnik@mail.ru>
2017-10-28 23:02:35 +0300

    Little fix

--- DataBase/View/SinglePersonForm.Designer.cs
+++ DataBase/View/SinglePersonForm.Designer.cs
@@ -104,7 +104,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(447, 137);
+            this.ClientSize = new System.Drawing.Size(447, 222);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.tbNewPhone);
             this.Controls.Add(this.lblPhones);
